07f38f5da19496d6721a5b73de9348b92f57e8b6
Roshan Panta <roshanpanta2059@gmail.com>
2022-09-10 00:13:52 +0545

    completed

--- lab1d.cs
+++ lab1d.cs
@@ -3,8 +3,10 @@ using System;
 class program{
     public static void Main(string[] args){
         int[] MyArray = {10,20,30,40,50};
-        for(int i = 0; i < MyArray.length; i++){
-            
+        int sum = 0;
+        for(int i = 0; i < MyArray.Length; i++){
+            sum = sum + MyArray[i];
         } 
+        Console.WriteLine("The sum is = " + sum);
     }
 }
