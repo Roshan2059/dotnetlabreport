546c19a8dd1ca6a327c0319e7074c097493af448
Roshan Panta <roshanpanta2059@gmail.com>
2022-09-10 00:08:17 +0545

    done upto for syntax

--- lab1d.cs
+++ lab1d.cs
@@ -2,6 +2,9 @@ using System;
 
 class program{
     public static void Main(string[] args){
-        
+        int[] MyArray = {10,20,30,40,50};
+        for(int i = 0; i < MyArray.length; i++){
+            
+        } 
     }
 }
